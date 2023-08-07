cbe7de3097113cd441849e6f6693c0a2613f7154
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-08-07 18:39:47 -0400

    commit tree 69a58b238ad673477118db49232b4bb101cbd180

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
+    validate :title_must_contain?
+    validates :title, presence: true
+    validates :content, length: { :minimum => 250}
+    validates :summary, length: { :maximum => 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+
+    
+
+    def title_must_contain?
+        # byebug
+       if title && !!title.match(/Won't Believe|Secret|Top \d*|Guess/)
+        true 
+       else 
+        errors.add(:title, "Must be clickbate")
+       end
+
+    end
 end
